1ea6cf44524bc7d9aec6b7eae431f81abc5cda35
Akilan <akilan2603@gmail.com>
2025-08-15 21:03:12 +0530

    deploy fixes

--- src/app/(admin)/tm-calendar/components/CalendarContainer.tsx
+++ src/app/(admin)/tm-calendar/components/CalendarContainer.tsx
@@ -74,7 +74,7 @@ type TransitMixer = {
   plant_id: string | null;
 };
 
-const timeSlots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+// const timeSlots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
 // Helper function to calculate duration between two times
 const calculateDuration = (start: string, end: string): number => {
